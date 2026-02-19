3bc717d582f7bb96ee8c1fc9221a2700396c6e13
Roshan Kohli <roshan.kohli.14.02@gmail.com>
2026-02-19 15:02:52 +0530

    Implement DriftMonitor class for monitoring data drift

--- ml_observability/monitor.py
+++ ml_observability/monitor.py
@@ -1,1 +1,36 @@
+import pandas as pd
+from .drift_metrics import calculate_psi, calculate_ks, calculate_js
 
+class DriftMonitor:
+    def __init__(self, segment_col=None):
+        self.segment_col = segment_col
+        self.train_df = None
+        self.model = None
+
+    def fit_baseline(self, train_df: pd.DataFrame):
+        self.train_df = train_df.copy()
+
+    def attach_model(self, model):
+        self.model = model
+
+    def monitor(self, prod_df: pd.DataFrame):
+        results = []
+
+        for feature in self.train_df.columns:
+            if feature == self.segment_col:
+                continue
+
+            psi = calculate_psi(self.train_df[feature], prod_df[feature])
+            ks_stat, ks_p = calculate_ks(self.train_df[feature], prod_df[feature])
+            js = calculate_js(self.train_df[feature], prod_df[feature])
+
+            results.append({
+                "feature": feature,
+                "PSI": psi,
+                "KS_pvalue": ks_p,
+                "JS_distance": js
+            })
+
+        drift_df = pd.DataFrame(results).sort_values("PSI", ascending=False)
+
+        return {"global_drift": drift_df}
